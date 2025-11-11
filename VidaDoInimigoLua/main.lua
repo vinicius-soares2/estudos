0658bdcabb2298f5fd996cb737e659db5cfbae8c
arch <l3rd0fail@gmail.com>
2025-11-11 10:16:22 -0300

    Arquivos Estudos

---
+++ VidaDoInimigoLua/main.lua
@@ -0,0 +1,13 @@
+local inimigo = {
+	nome = "Slime Verde",
+	vida = 50,
+	dano = 8
+}
+
+local function atacar(inimigo, danoRecebido)
+	inimigo.vida = inimigo.vida - danoRecebido
+	print(string.format("%s agora tem %d de vida. ", inimigo.nome, inimigo.vida))
+end
+
+--chamando a funcao
+atacar(inimigo, 8)
